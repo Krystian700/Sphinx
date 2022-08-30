8cf99e05a276ef3dac5a4af0ceed77e738fea9f2
Krystian700 <ice_devil@wp.pl>
2022-08-30 20:42:28 +0200

    Test 5 rozgalezienie skryptu make

--- make.bat
+++ make.bat
@@ -4,6 +4,11 @@ set /p wybor=T/N
 if /I "%wybor%"=="T" goto Tak
 goto Nie
 
+:Tak
+
+echo Podaj nazwe commita:
+set /p Commit=
+
 pushd %~dp0
 
 REM Command file for Sphinx documentation
@@ -40,10 +45,6 @@ popd
 
 xcopy C:\Programy\GIT\Sphinx\build\html\*.* c:\Programy\GIT\Strona /E /H
 
-:Tak
-echo Podaj nazwe commita:
-set /p Commit=
-
 git add . 
 git commit -m "%Commit%"
 git push
@@ -55,4 +56,38 @@ git commit -m "%Commit%"
 git push
 
 :Nie
-pause
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+set SOURCEDIR=source
+set BUILDDIR=build
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
+	echo.If you don't have Sphinx installed, grab it from
+	echo.https://www.sphinx-doc.org/
+	exit /b 1
+)
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+
+:end
+popd
+
+xcopy C:\Programy\GIT\Sphinx\build\html\*.* c:\Programy\GIT\Strona /E /H
